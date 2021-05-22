72c8f128dc3a168730d214be76054d7430a99ed8
exel limartha <exel.lim@gmail.com>
2021-05-22 10:37:17 +0700

    complete

---
+++ resources/views/artikel.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>1461900256 Exelensi Christian Simon Limartha</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<div class="topnav" id="myTopnav">
+  <a href="/home">Home</a>
+  <a href="/artikel" class="active">Artikel</a>
+  <a href="/contact">Contact Us</a>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
+</head>
+<body>
+
+<h1>Welcome to the Article Page</h1>
+<h2>Apa itu Framework?</h2>
+<p>Framework adalah kerangka kerja yang berfungsi untuk mengembangkan aplikasi berbasis web dan desktop.
+Framework membnatu developer dalam menuliskan kode program agar lebih terstruktur dan rapi.</p>
+
+</body>
+<style> /*css */
+/* Add a black background color to the top navigation */
+.topnav {
+  background-color: #333;
+  overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Add an active class to highlight the current page */
+.topnav a.active {
+  background-color: #04AA6D;
+  color: white;
+}
+
+/* Hide the link that should open and close the topnav on small screens */
+.topnav .icon {
+  display: none;
+}
+</style>
+
+<script> /* Java Script */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+</script>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ArtikelController;
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +18,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/home' , [HomeController::class, 'index']);
+Route::get('/artikel', function () {
+    return view('artikel');
+});
+Route::get('/contact', function () {
+    return view('contact');
 });
